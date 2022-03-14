8350cbfbeda2f34f5814add665339f67e927b39d
hidri khalil <hidrikhalil9@gmail.com>
2022-03-14 08:50:01 +0100

    Adding models and BookRepository

---
+++ DAL/Repository/BookRepository.cs
@@ -0,0 +1,19 @@
+﻿using DAL.DBContext;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace DAL.Repository
+{
+    public class BookRepository
+    {
+        public IEnumerable<Book> Get()
+        {
+            using(var context = new DemoDBContext())
+            {
+                return context.Books.ToList();
+            }
+        }
+
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Models
+{
+    public partial class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int AuthorId { get; set; }
+        public int PublisherId { get; set; }
+        public int LibraryId { get; set; }
+
+        public virtual Author Author { get; set; }
+        public virtual Library Library { get; set; } 
+        public virtual Publisher Publisher { get; set; }
+    }
+}
